955678393e8279e637ee2d52cae97db9be4b7afa
hafeezullah99 <hafeez.ullah@joybox.pk>
2024-05-25 23:21:52 +0500

    fix null value error in chats

--- lib/views/chat_screen/chat_screen.dart
+++ lib/views/chat_screen/chat_screen.dart
@@ -1,19 +1,15 @@
-import 'dart:convert';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:demo_project1/common_widgets/common_appbar.dart';
 import 'package:demo_project1/views/chat_screen/ChatDetailPage.dart';
 import 'package:demo_project1/views/chat_screen/provider/chat_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_profile_picture/flutter_profile_picture.dart';
-import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../services/chat/chat_services.dart';
 import 'model/chat_user_model.dart';
-import 'model/search_user.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -31,24 +27,40 @@ class _ChatScreenState extends State<ChatScreen> {
   TextEditingController _messageController = TextEditingController();
   late SharedPreferences prefs;
   String? userID;
+  bool isLoading = true;
 
   ChatServices chatServices = new ChatServices();
 
   @override
   void initState() {
-    initSharedPreferences();
     super.initState();
+    initSharedPreferences();
   }
 
   Future<void> initSharedPreferences() async {
     prefs = await SharedPreferences.getInstance();
     setState(() {
       userID = prefs.getString('userid');
+      isLoading = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    if (isLoading) {
+      return const Scaffold(
+        appBar: CommonAppBar(title: "chat", showicon: true),
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    if (userID == null) {
+      return const Scaffold(
+        appBar: CommonAppBar(title: "chat", showicon: true),
+        body: Center(child: Text("User ID not found")),
+      );
+    }
+
     return Scaffold(
       appBar: const CommonAppBar(title: "chat", showicon: true),
       body: Column(
@@ -56,99 +68,96 @@ class _ChatScreenState extends State<ChatScreen> {
         children: <Widget>[
           _buildSearch(),
           Flexible(
-            child: StreamBuilder<String?>(
-              stream: ChatServices().getUserIDStream(),
-              builder: (context, snapshot) => StreamBuilder<List<ChatUser>>(
-                stream: ChatServices().chattedUsersStream(userID!, context),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError) {
-                    return const Center(
-                      child: Text(
-                        "No chats Available",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                    );
-                  } else {
-                    List<ChatUser> chattedUsers = snapshot.data ?? [];
-                    return ListView.builder(
-                      itemCount: chattedUsers.length,
-                      itemBuilder: (context, index) {
-                        final user = chattedUsers[index];
-                        return ListTile(
-                          leading: SizedBox(
-                            width: 48,
-                            height: 48,
-                            child: user.userImage == ""
-                                ? const CircleAvatar(
-                                    backgroundImage: CachedNetworkImageProvider(
-                                        "https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTAxL3JtNjA5LXNvbGlkaWNvbi13LTAwMi1wLnBuZw.png"),
-                                  )
-                                : ProfilePicture(
-                                    name: user.userName.isNotEmpty
-                                        ? user.userName
-                                        : "User",
-                                    fontsize: 16,
-                                    radius: 24,
-                                    random: true,
-                                  ),
-                          ),
-                          title: Text(user.userName.toUpperCase()),
-                          subtitle: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(user.lastMessage),
-                            ],
-                          ),
-                          trailing: Column(
-                            children: [
-                              const SizedBox(
-                                height: 8,
-                              ),
-                              Text(
-                                _formatTimestamp(user.timestamp),
-                                style: const TextStyle(color: Colors.grey),
-                              ),
-                              user.unreadMessages > 0
-                                  ? Container(
-                                      width: 20,
-                                      decoration: BoxDecoration(
-                                          color: Colors.red,
-                                          borderRadius:
-                                              BorderRadius.circular(20)),
-                                      child: Center(
-                                        child: Text(
-                                          (user.unreadMessages.toString()),
-                                          style: const TextStyle(
-                                              color: Colors.white),
-                                        ),
-                                      ),
-                                    )
-                                  : const Text(''),
-                            ],
-                          ),
-                          onTap: () {
-                            setState(() {});
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ChatDetailPage(
-                                  receiverID: user.receiverID,
-                                  senderID: userID!,
-                                  userImage: user.userImage,
-                                  userName: user.userName,
+            child: StreamBuilder<List<ChatUser>>(
+              stream: ChatServices().chattedUsersStream(userID!, context),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return const Center(
+                    child: Text(
+                      "No chats Available",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  );
+                } else {
+                  List<ChatUser> chattedUsers = snapshot.data ?? [];
+                  return ListView.builder(
+                    itemCount: chattedUsers.length,
+                    itemBuilder: (context, index) {
+                      final user = chattedUsers[index];
+                      return ListTile(
+                        leading: SizedBox(
+                          width: 48,
+                          height: 48,
+                          child: user.userImage == ""
+                              ? const CircleAvatar(
+                                  backgroundImage: CachedNetworkImageProvider(
+                                      "https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTAxL3JtNjA5LXNvbGlkaWNvbi13LTAwMi1wLnBuZw.png"),
+                                )
+                              : ProfilePicture(
+                                  name: user.userName.isNotEmpty
+                                      ? user.userName
+                                      : "User",
+                                  fontsize: 16,
+                                  radius: 24,
+                                  random: true,
                                 ),
+                        ),
+                        title: Text(user.userName.toUpperCase()),
+                        subtitle: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(user.lastMessage),
+                          ],
+                        ),
+                        trailing: Column(
+                          children: [
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              _formatTimestamp(user.timestamp),
+                              style: const TextStyle(color: Colors.grey),
+                            ),
+                            user.unreadMessages > 0
+                                ? Container(
+                                    width: 20,
+                                    decoration: BoxDecoration(
+                                        color: Colors.red,
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                    child: Center(
+                                      child: Text(
+                                        (user.unreadMessages.toString()),
+                                        style: const TextStyle(
+                                            color: Colors.white),
+                                      ),
+                                    ),
+                                  )
+                                : const Text(''),
+                          ],
+                        ),
+                        onTap: () {
+                          setState(() {});
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ChatDetailPage(
+                                receiverID: user.receiverID,
+                                senderID: userID!,
+                                userImage: user.userImage,
+                                userName: user.userName,
                               ),
-                            );
-                          },
-                        );
-                      },
-                    );
-                  }
-                },
-              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  );
+                }
+              },
             ),
           ),
         ],
@@ -185,121 +194,6 @@ class _ChatScreenState extends State<ChatScreen> {
       ),
     );
   }
-
-  void _showUserListDialog(BuildContext context) async {
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return const AlertDialog(
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              CircularProgressIndicator(),
-              SizedBox(height: 16),
-              Text('Fetching chat users...'),
-            ],
-          ),
-        );
-      },
-    );
-
-    TextEditingController searchController = TextEditingController();
-    final List<SearchUsers> allUsers =
-        await fetchChatUsers(userID!, "", context);
-
-    Navigator.pop(context);
-
-    List<SearchUsers> displayedUsers = List.from(allUsers);
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return StatefulBuilder(
-          builder: (context, setState) {
-            return AlertDialog(
-              title: const Text('Chat Users'),
-              content: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextField(
-                    controller: searchController,
-                    decoration: const InputDecoration(
-                      labelText: 'Search',
-                      prefixIcon: Icon(Icons.search),
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        displayedUsers = allUsers
-                            .where((user) => user.adminName
-                                .toLowerCase()
-                                .contains(value.toLowerCase()))
-                            .toList();
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  displayedUsers.isEmpty
-                      ? const Text('No users found')
-                      : Expanded(
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: displayedUsers.length,
-                            itemBuilder: (context, index) {
-                              final user = displayedUsers[index];
-
-                              return SizedBox(
-                                width: 100,
-                                child: ListTile(
-                                  leading: SizedBox(
-                                    width: 48,
-                                    height: 48,
-                                    child: user.image == null ||
-                                            !user.image.endsWith("/")
-                                        ? CircleAvatar(
-                                            backgroundImage:
-                                                CachedNetworkImageProvider(
-                                                    user.image),
-                                          )
-                                        : ProfilePicture(
-                                            name: user.adminName,
-                                            fontsize: 16,
-                                            radius: 24,
-                                            random: true,
-                                          ),
-                                  ),
-                                  title: Text(user.adminName),
-                                  onTap: () {
-                                    Navigator.pop(context);
-                                    _navigateToUserScreen(context, user);
-                                  },
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                ],
-              ),
-            );
-          },
-        );
-      },
-    );
-  }
-
-  void _navigateToUserScreen(BuildContext context, SearchUsers user) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => ChatDetailPage(
-          userName: user.adminName,
-          userImage: user.image,
-          receiverID: user.id,
-          senderID: userID ?? "1",
-        ),
-      ),
-    );
-  }
 }
 
 class ChatUsers {
@@ -318,37 +212,6 @@ class ChatUsers {
     required this.secondaryText,
     this.unreadMessages = 0,
   });
-}
-
-Future<List<SearchUsers>> fetchChatUsers(
-    String userID, String search, BuildContext context) async {
-  try {
-    final response = await http.post(
-      Uri.parse('https://alkhandaq-b.s-tip.com/stip_api/Api/chat_userlist'),
-      body: {
-        'userID': userID,
-        'search': search,
-      },
-    );
-
-    if (response.statusCode == 200) {
-      final jsonData = jsonDecode(response.body);
-      final List<SearchUsers> users = [];
-
-      jsonData['data'].forEach((key, value) {
-        final List<dynamic> userList = value;
-        userList.forEach((user) {
-          users.add(SearchUsers.fromJson(user));
-        });
-      });
-
-      return users;
-    } else {
-      throw Exception('Failed to load chat users: ${response.statusCode}');
-    }
-  } catch (error) {
-    throw Exception('Failed to load chat users: $error');
-  }
 }
 
 String _formatTimestamp(DateTime timestamp) {
